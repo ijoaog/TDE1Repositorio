08ebb91b4933133291a3bf49896e70ced009f326
ijoaog <66679956+ijoaog@users.noreply.github.com>
2021-09-21 12:23:52 -0300

    lab2 = lab
    como lab2 é igual ao lab, não precisa repetir todo o vector;

--- main.cpp
+++ main.cpp
@@ -32,28 +32,7 @@ int main() {
 	"XXXXXXXXXXXXXXXXXXXSX"
 	};
 // Com limpeza dos asteriscos
-	vector<string> lab2 = {
-	"XXXXXXXXXXXXXXXXXXXXX",
-	"X     X     X     X X",
-	"XX XX XXXXX X X X   X",
-	"X   X       XXX XXX X",
-	"X X X XXXXXXX   X   X",
-	"X X   X   X X X   X X",
-	"X XXXXX X X   XXXXX X",
-	"X X     X XXX  X    X",
-	"X X X X X   XX XX XXX",
-	"X X XXX X X     X   X",
-	"E X X X X XXXXX XXX X",
-	"X     X X   X X X   X",
-	"X XXXXX XXX X XXX X X",
-	"X X       X X   X X X",
-	"X   XXX X X XXX X X X",
-	"XXX X X X X X X X XXX",
-	"X X   X X   X   X   X",
-	"X XXX XXXXXXXXX XXX X",
-	"X                 X X",
-	"XXXXXXXXXXXXXXXXXXXSX"
-	};
+	vector<string> lab2 = lab;
 
 	// Entrada das variáveis
 	int x = 10;
